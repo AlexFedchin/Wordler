6cfa25cacfab308c06313f3d5702fb450d1c80d6
Alexander <alexfedchin22@gmail.com>
2024-06-27 16:29:39 +0300

    Complete the Word List editing feature

--- frontend/src/pages/WordListEditPage.jsx
+++ frontend/src/pages/WordListEditPage.jsx
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { Container, Box, IconButton } from "@mui/material";
+import { Container, Box, IconButton, Typography } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 import config from "../config";
 import SectionSubheading from "../components/SectionSubheading";
-import UnderlinedTextField from "../components/UnderlinedTextField"; // Import the new component
+import UnderlinedTextField from "../components/UnderlinedTextField";
 import FilledButton from "../components/FilledButton";
+import {
+  validateWordListForm,
+  validateWordListLanguage,
+  validateWordListTitle,
+} from "../utils/validation";
 
 const WordListEditPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [wordList, setWordList] = useState(null);
   const [title, setTitle] = useState("");
-  const [originLanguage, setOriginLanguage] = useState("");
-  const [targetLanguage, setTargetLanguage] = useState("");
+  const [nativeLanguage, setNativeLanguage] = useState("");
+  const [foreignLanguage, setForeignLanguage] = useState("");
   const [words, setWords] = useState([]);
 
   useEffect(() => {
@@ -26,8 +32,8 @@ const WordListEditPage = () => {
         const list = response.data;
         setWordList(list);
         setTitle(list.title);
-        setOriginLanguage(list.originLanguage);
-        setTargetLanguage(list.targetLanguage);
+        setNativeLanguage(list.nativeLanguage);
+        setForeignLanguage(list.foreignLanguage);
         setWords(list.words);
       } catch (error) {
         console.error("Error fetching word list:", error);
@@ -38,7 +44,7 @@ const WordListEditPage = () => {
   }, [id]);
 
   const handleAddWord = () => {
-    setWords([...words, { originWord: "", targetWord: "" }]);
+    setWords([...words, { nativeWord: "", foreignWord: "" }]);
   };
 
   const handleWordChange = (index, field, value) => {
@@ -47,12 +53,17 @@ const WordListEditPage = () => {
     setWords(newWords);
   };
 
+  const handleDeleteWord = (index) => {
+    const newWords = words.filter((_, wordIndex) => wordIndex !== index);
+    setWords(newWords);
+  };
+
   const handleSave = async () => {
     try {
       await axios.put(`${config.API_BASE_URL}wordlists/${id}`, {
         title,
-        originLanguage,
-        targetLanguage,
+        nativeLanguage,
+        foreignLanguage,
         words,
       });
       navigate("/wordLists");
@@ -78,86 +89,202 @@ const WordListEditPage = () => {
         alignItems: "center",
       }}
     >
+      {/* Title of the list */}
       <Box width={"40%"}>
         <UnderlinedTextField
           label="List Title"
-          centered="true"
+          centered={true}
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        {validateWordListTitle(title) !== "" && (
+          <Typography
+            align="left"
+            width={"100%"}
+            sx={{
+              fontSize: "large",
+              color: "var(--error-color)",
+              fontFamily: "TextFont",
+            }}
+          >
+            {validateWordListTitle(title)}
+          </Typography>
+        )}
       </Box>
 
       <Box sx={{ width: "100%", marginTop: 2 }}>
+        {/* Row with languages */}
         <Box sx={{ display: "flex", gap: 10, marginBottom: 2 }}>
-          <UnderlinedTextField
-            label="Origin Language"
-            value={originLanguage}
-            onChange={(e) => setOriginLanguage(e.target.value)}
-          />
-          <UnderlinedTextField
-            label="Target Language"
-            value={targetLanguage}
-            onChange={(e) => setTargetLanguage(e.target.value)}
-          />
+          <Box width={"50%"}>
+            <UnderlinedTextField
+              label="Native Language"
+              value={nativeLanguage}
+              centered={true}
+              onChange={(e) => setNativeLanguage(e.target.value)}
+            />
+            {validateWordListLanguage(nativeLanguage) !== "" && (
+              <Typography
+                align="left"
+                width={"100%"}
+                sx={{
+                  fontSize: "large",
+                  color: "var(--error-color)",
+                  fontFamily: "TextFont",
+                }}
+              >
+                {validateWordListLanguage(nativeLanguage)}
+              </Typography>
+            )}
+          </Box>
+          <Box width={"50%"}>
+            <UnderlinedTextField
+              label="Foreign Language"
+              value={foreignLanguage}
+              centered={true}
+              onChange={(e) => setForeignLanguage(e.target.value)}
+            />
+            {validateWordListLanguage(foreignLanguage) !== "" && (
+              <Typography
+                align="left"
+                width={"100%"}
+                sx={{
+                  fontSize: "large",
+                  color: "var(--error-color)",
+                  fontFamily: "TextFont",
+                }}
+              >
+                {validateWordListLanguage(foreignLanguage)}
+              </Typography>
+            )}
+          </Box>
         </Box>
+        {/* Rows for every word pair in the list */}
         {words.map((word, index) => (
-          <Box key={index} sx={{ display: "flex", gap: 20, marginBottom: 2 }}>
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: 7,
+              marginBottom: 2,
+              "&:hover .delete-icon": {
+                opacity: 1,
+              },
+            }}
+          >
             <UnderlinedTextField
               label=""
-              value={word.originWord}
+              value={word.nativeWord}
               onChange={(e) =>
-                handleWordChange(index, "originWord", e.target.value)
+                handleWordChange(index, "nativeWord", e.target.value)
               }
             />
+            <IconButton
+              className="delete-icon"
+              onClick={() => handleDeleteWord(index)}
+              sx={{
+                opacity: 0,
+                transition: "all 0.3s ease",
+                color: "var(--off-white-color)",
+                "&:hover": {
+                  color: "var(--error-color)",
+
+                  filter:
+                    "drop-shadow(0 0 4px var(--error-color)) drop-shadow(0 0 16px var(--error-color))",
+                },
+              }}
+            >
+              <DeleteIcon />
+            </IconButton>
             <UnderlinedTextField
               label=""
-              value={word.targetWord}
+              value={word.foreignWord}
               onChange={(e) =>
-                handleWordChange(index, "targetWord", e.target.value)
+                handleWordChange(index, "foreignWord", e.target.value)
               }
             />
           </Box>
         ))}
       </Box>
       {/* Plus icon to add a new list */}
-      <IconButton
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignSelf: "center",
-        }}
-        onClick={handleAddWord}
-      >
-        <AddCircleOutlineIcon
+      {words.length < config.wordListMaxLength && (
+        <IconButton
           sx={{
-            color: "var(--accent-color)",
-            fontSize: "64px",
-            transition: "all 0.3s ease",
-            "&:hover": {
-              filter:
-                "drop-shadow(0 0 4px var(--accent-color)) drop-shadow(0 0 18px var(--accent-color))",
-            },
+            display: "flex",
+            justifyContent: "center",
+            alignSelf: "center",
+            mt: 2,
           }}
-        />
-      </IconButton>
+          onClick={handleAddWord}
+        >
+          <AddCircleOutlineIcon
+            sx={{
+              color: "var(--accent-color)",
+              fontSize: "64px",
+              transition: "all 0.3s ease",
+              "&:hover": {
+                filter:
+                  "drop-shadow(0 0 4px var(--accent-color)) drop-shadow(0 0 18px var(--accent-color))",
+              },
+            }}
+          />
+        </IconButton>
+      )}
+
       <Box
         sx={{
           display: "flex",
+          flexDirection: "row",
           justifyContent: "space-between",
           width: "100%",
-          marginTop: 4,
+          mt: 4,
+          mb: 8,
         }}
       >
         <FilledButton
           onClick={handleCancel}
           width={"49%"}
+          height={"60px"}
           mainColor="var(--error-color)"
         >
           Cancel
         </FilledButton>
-        <FilledButton onClick={handleSave} width={"49%"}>
-          Save
-        </FilledButton>
+        <Box
+          sx={{
+            width: "49%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <FilledButton
+            onClick={handleSave}
+            width={"100%"}
+            height={"60px"}
+            disabled={
+              validateWordListForm(words) !== "" ||
+              validateWordListTitle(title) !== "" ||
+              validateWordListLanguage(nativeLanguage) !== "" ||
+              validateWordListLanguage(foreignLanguage) !== ""
+            }
+          >
+            Save
+          </FilledButton>
+          {validateWordListForm(words) !== "" && (
+            <Typography
+              align="left"
+              width="100%"
+              sx={{
+                fontSize: "large",
+                color: "var(--error-color)",
+                fontFamily: "TextFont",
+                mt: 2,
+              }}
+            >
+              {validateWordListForm(words)}
+            </Typography>
+          )}
+        </Box>
       </Box>
     </Container>
   );
