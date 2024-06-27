a97dda8537b606384959e8d8409d925cdbb4e8dc
Alexander <alexfedchin22@gmail.com>
2024-06-27 16:32:41 +0300

    Add global limitations for word lists data

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,7 +3,11 @@ const config = {
   passwordMinLength: 7,
   passwordMaxLength: 25,
   nicknameMinLength: 4,
-  nicknameMaxLength: 12,
+  nicknameMaxLength: 15,
+  wordListMinLength: 5,
+  wordListMaxLength: 30,
+  wordListTitleMaxLength: 20,
+  wordListLanguageMaxLength: 20,
 };
 
 export default config;
