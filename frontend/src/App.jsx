a8d839509f0593a8df07974bba171c663df0029e
AlexFedchin <alexanderfedchin@gmail.com>
2024-07-12 16:25:42 +0300

    Remove global padding to make full screen lines available

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Play from "./pages/Play";
-import Profile from "./pages/Profile";
-import WordLists from "./pages/WordLists";
-import WordListEditPage from "./pages/WordListEditPage";
-import Registration from "./pages/Registration";
-import { Box } from "@mui/material";
-import { UserProvider } from "./UserContext";
-import CustomAppBar from "./components/CustomAppBar";
-import "./styles/app.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Play from './pages/Play';
+import Profile from './pages/Profile';
+import WordLists from './pages/WordLists';
+import WordListEditPage from './pages/WordListEditPage';
+import Registration from './pages/Registration';
+import { Box } from '@mui/material';
+import { UserProvider } from './UserContext';
+import CustomAppBar from './components/CustomAppBar';
+import './styles/app.css';
 
 function App() {
   return (
@@ -21,11 +21,9 @@ function App() {
           <Box
             sx={{
               mt: 10,
-              width: "100%",
-              paddingLeft: "2%",
-              paddingRight: "2%",
-              alignContent: "center",
-              boxSizing: "border-box",
+              width: '100%',
+              alignContent: 'center',
+              boxSizing: 'border-box',
             }}
           >
             <Routes>
